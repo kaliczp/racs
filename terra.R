b5eb2d88e036669b9f4f06c089b8afd3cff224f5
Péter Kalicz <kaliczp@gmail.com>
2022-11-30 17:04:38 +0100

    Change ido name plot yearly max and save result

--- terra.R
+++ terra.R
@@ -2,7 +2,11 @@ library(terra)
 full.terra <- rast(cbind(koord[,c("Lambda","Fi")], t(ttest2.m)), type="xyz")
 
 library(rts)
-full.rts <- rts(full.terra, ttido)
+full.rts <- rts(full.terra, ido)
 
 maxyr.rts <- apply.yearly(full.rts, max)
 
+## Éves maximum
+plot(maxyr.rts['2001'])
+
+eves.terra <- maxyr.rts@raster
