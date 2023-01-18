f002822518ae05a59f4b8fdf5423526e1722e8ab
Péter Kalicz <kaliczp@gmail.com>
2023-01-18 15:45:19 +0100

    Two variables plot and regression

--- tobbvalt.R
+++ tobbvalt.R
@@ -1,3 +1,8 @@
-x <- 1:100
-y <- 1:100
+x <- sample(1:100, size = 200, replace = TRUE)
+y <- sample(1:100, size = 200, replace = TRUE)
 tv.df <- data.frame(x, y)
+## plot
+plot(tv.df, asp = TRUE)
+
+tv.lm <- lm(y ~ x, tv.df)
+summary(tv.lm)
