cd96c35dc5c2a5e88a9d9cca1d120f386027386c
Péter Kalicz <kaliczp@gmail.com>
2023-01-18 17:30:06 +0100

    Identify possibly invalid data and replay lm() + calculate correlations

--- Kunpeszer.R
+++ Kunpeszer.R
@@ -16,3 +16,19 @@ pH.lm <- lm(pH_H2O ~ pH_KCL, Kunp)
 abline(pH.lm)
 boxplot(resid(pH.lm))
 plot(pH.lm)
+
+## Gyanús pH-k
+plot(pH_H2O ~ pH_KCL, Kunp)
+abline(0,1)
+identify(Kunp[,c("pH_KCL","pH_H2O")])
+## Az azonosítottak kizárása
+Kunp1 <- Kunp[-c(508, 566, 1330, 2366),]
+plot(pH_H2O ~ pH_KCL, Kunp1)
+abline(0,1)
+
+## Modell újra
+plot(pH_H2O ~ pH_KCL, Kunp1)
+pH1.lm <- lm(pH_H2O ~ pH_KCL, Kunp1)
+abline(pH1.lm)
+cor(Kunp1$pH_H2O, Kunp1$pH_KCL)
+cor(Kunp1[,-c(1:5,14,17:18)])
